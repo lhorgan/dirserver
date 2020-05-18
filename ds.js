819d8a7b48d1158f20d4dec224db6abe62c74496
Luke Horgan <horgan.l@husky.neu.edu>
2020-05-18 11:22:58 -0400

    fix it up a bit

--- ds.js
+++ ds.js
@@ -8,6 +8,11 @@ const getDirectories = source =>
 var express = require('express')
 var app = express()
 
+const express = require('express')
+const app = express()
+const port = 3000
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
     res.send('hello world')
